f5a9fbd5938c0cbe4896934218c255cdb10e164a
hudsonk2 <hudsonk2@seattleu.edu>
2016-11-20 19:38:02 -0500

    made new stuff and updated it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$(window).scroll(function(){
+    $(".text-center").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+$(window).scroll(function(){
+    $(".carousel-inner").css("opacity", 1 - $(window).scrollTop() / 950);
+  });
